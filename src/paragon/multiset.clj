cd81c426710561609cdfc8a1ea14a4722b649fcb
winsome <winsome@idle.horse>
2020-07-10 23:41:53 -0500

    Spec and instrument multisets

--- src/paragon/multiset.clj
+++ src/paragon/multiset.clj
@@ -1,7 +1,11 @@
 (ns paragon.multiset
-  (:require [clojure.pprint :as pprint]))
+  (:require [clojure.pprint :as pprint]
+            [clojure.spec.alpha :as s]
+            [orchestra.spec.test :as st])
+  (:import (java.util Collection)))
 
 (defprotocol Multiplicities
+  ;; multiplicities is just a frequencies map
   (multiplicities [this]))
 
 (deftype Multiset [^clojure.lang.IPersistentMap meta
@@ -82,6 +86,13 @@
   Multiplicities
   (multiplicities [this] multiplicities))
 
+(defn multiset?
+  [x]
+  (instance? Multiset x))
+(s/fdef multiset?
+  :args (s/cat :x any?)
+  :ret boolean?)
+
 (defmethod print-method Multiset
   [^Multiset mset ^java.io.Writer w]
   (.write w (.toString mset)))
@@ -96,21 +107,47 @@
    (->Multiset nil {} 0))
   ([s]
    (->Multiset nil (frequencies s) (count s))))
+(s/fdef multiset
+  :args (s/alt :arity-0 (s/cat)
+               :arity-1 (s/cat :items coll?))
+  :ret multiset?)
 
 (defn mset
   "Create a multiset."
   ([]
-   {})
+   (multiset))
   ([& xs]
    (multiset xs)))
+(s/fdef mset
+  :args (s/cat :items (s/* any?))
+  :ret multiset?)
 
 (defn multiset-reader
   [x]
   (multiset x))
+(s/fdef multiset-reader
+  :args (s/cat :x coll?)
+  :ret multiset?)
 
-(defn multiset?
-  [x]
-  (instance? Multiset x))
+(defn subset?
+  "Returns true if ms1 is a subset of ms2."
+  [ms1 ms2]
+  (let [a (multiplicities ms1)
+        b (multiplicities ms2)]
+    (reduce (fn [res x]
+              (if res
+                (and res (<= (get a x) (get b x 0)))
+                (reduced false)))
+            true
+            ms1)))
+(s/fdef subset?
+  :args (s/cat :ms1 multiset? :ms2 multiset?)
+  :ret boolean?)
+
+(comment
+  (subset? #mset [1 2 3] #mset [1 1 2 2 ])
+  (subset? #mset [1 2 3] #mset [1 1 2 2 3])
+  )
 
 (defn union
   "Returns a multiset that is the result of unioning the multisets together."
@@ -119,12 +156,35 @@
           result ms1]
      (if (seq entry)
        (let [[k v] entry]
+         ;; Getting rid of metadata because this is a new collection
          (recur rest (->Multiset nil
                                  (update (multiplicities result) k (fnil (partial + v) 0))
                                  (+ (count result) v))))
        result)))
   ([ms1 ms2 & multisets]
    (reduce union ms1 (clojure.core/conj multisets ms2))))
+(s/fdef union
+  :args (s/alt :arity-2 (s/cat :ms1 multiset? :ms2 multiset?)
+               :variadic (s/cat :ms1 multiset? :ms2 multiset? :multisets (s/* multiset?)))
+  :ret multiset?
+  ;; each input set must be a subset of the output
+  :fn #(let [{:keys [ms1 ms2 multisets]} (-> % :args second)
+             inputs (conj (or multisets []) ms1 ms2)
+             ret (:ret %)]
+         (reduce (fn [acc arg]
+                   (if acc
+                     (and acc (subset? arg ret))
+                     (reduced false)))
+                 true
+                 inputs)))
+
+(comment
+  {:ret #mset [4 1 2 3], :args [:arity-2 {:ms1 #mset [4], :ms2 #mset [1 2 3]}]}
+  {:ret #mset [4 1 2 3], :args [:arity-2 {:ms1 #mset [4], :ms2 #mset [1 2 3]}]}
+  (union #mset [1] #mset [2] #mset [3] )
+
+
+  )
 
 (defn intersection
   "Returns only the common keys of the multisets."
@@ -137,6 +197,7 @@
          (let [[k v2] entry
                v1 (get ms1 k 0)
                new-v (min v1 v2)]
+           ;; Getting rid of metadata because this is a new collection
            (recur rest (->Multiset nil (if (zero? new-v)
                                          (dissoc (multiplicities result) k)
                                          (assoc (multiplicities result) k new-v))
@@ -145,6 +206,20 @@
   ([ms1 ms2 & multisets]
    (let [smallest-first (sort-by count (conj multisets ms1 ms2))]
      (reduce intersection (first smallest-first) (rest smallest-first)))))
+(s/fdef intersection
+  :args (s/alt :arity-2 (s/cat :ms1 multiset? :ms2 multiset?)
+               :variadic (s/cat :ms1 multiset? :ms2 multiset? :multisets (s/* multiset?)))
+  :ret multiset?
+  ;; the return set must be a subset of each input
+  :fn #(let [{:keys [ms1 ms2 multisets]} (-> % :args second)
+             inputs (conj (or multisets []) ms1 ms2)
+             ret (:ret %)]
+         (reduce (fn [acc arg]
+                   (if acc
+                     (and acc (subset? ret arg))
+                     (reduced false)))
+                 true
+                 inputs)))
 
 (comment
   (count (intersection #mset [1 1 2 3] #mset [1 4]))
@@ -153,12 +228,29 @@
 (defn difference
   "Returns the keys in ms1 that are not in the following multisets."
   ([ms1 ms2]
-   (reduce disj ms1 (keys (multiplicities ms2))))
+   (reduce disj ms1 ms2))
   ([ms1 ms2 & multisets]
    (reduce difference ms1 (conj multisets ms2))))
+(s/fdef difference
+  :args (s/alt :arity-2 (s/cat :ms1 multiset? :ms2 multiset?)
+               :variadic (s/cat :ms1 multiset? :ms2 multiset? :multisets (s/* multiset?)))
+  :ret multiset?
+  ;; the return set must be a subset of ms1 and not be a subset of any other set
+  :fn #(let [{:keys [ms1 ms2 multisets]} (-> % :args second)
+             diff-inputs (conj (or multisets []) ms2) ; excluding ms1
+             ret (:ret %)]
+         (reduce (fn [acc arg]
+                   (if acc
+                     (and acc (subset? ret ms1) (not (subset? ret arg)))
+                     (reduced false)))
+                 true
+                 diff-inputs)))
 
 (comment
-  (difference #mset [] #mset [1 1 3])
+  (difference #mset [1 1 2 3] #mset [1 1 3])
+  #mset [2]
+  #mset [1 2]
+  #mset []
 
   )
 
@@ -167,6 +259,9 @@
   [ms1 ms2]
   (multiset (for [a (seq ms1) b (seq ms2)]
               [a b])))
+(s/fdef product
+  :args (s/cat :ms1 multiset? :ms2 multiset?)
+  :ret multiset?)
 
 (comment
   (multiset [1 2 3 1 2 3 nil true false])
@@ -214,7 +309,5 @@
   (concat
    {1 5, 3 1}
    {1 3, 3 1})
-  ([1 5] [3 1] [1 3] [3 1])
-
-
-  )
+  ([1 5] [3 1] [1 3] [3 1]))
+(st/instrument)
