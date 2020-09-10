ee5834884344b8a5af5c4dd9aadda477cba001ab
winsome <winsome@idle.horse>
2020-09-09 22:26:28 -0500

    Add broken sequence implementation
    This still doesn't work because the uuid needs to be assigned at element insertion time.

--- src/paragon/components/mrdt/mrdt.clj
+++ src/paragon/components/mrdt/mrdt.clj
@@ -46,9 +46,12 @@
 
 (defn seq->relations
   [s]
-  (let [uniquified (uniquify-seq s)]
-    {:mem (build-mem uniquified)
-     :ord (build-ob uniquified)}))
+  (let [val-ids (map (fn [x] (java.util.UUID/randomUUID)) s)]
+    {:mem (build-mem val-ids)
+     :ord (build-ob val-ids)
+     ;; TODO: you can't map the values at abstraction time, you need to store that with
+     ;; the the actual sequence
+     :val-map (zipmap val-ids s)}))
 
 (comment
   [1 2 1] -> [[1 1] [2 1] [1 2]]
@@ -147,24 +150,6 @@
            :else
            []))))
 
-
-(comment
-  (bfs b 3 1)
-  (1 2 3)
-  nil
-  ()
-  [1]
-  (1 5 6)
-  (2 3 4)
-  (1 2 3 4)
-  (1 2 3)
-  (1 2 3)
-  [[[[] nil] nil]]
-  []
-  [[]]
-
-  )
-
 (defn prune
   "Remove edge (u,v) if there exists edges (u, w) and (w, v)."
   [g]
@@ -190,95 +175,20 @@
                          (partition 2 1 incoming-edges))) ; break edges into pairs
                g)))
 
-(comment
-  (def edges [[1 2] [2 3] [3 4] [1 4] [1 3] [2 4]])
-  (def a (reduce add-edge {} edges))
-  (def edges2 [[1 2] [2 3] [3 4] [1 5] [5 6] [6 7]])
-  (def b (build-graph edges2))
-  (def edges3 [[1 2] [1 3]])
-  (def c (build-graph edges3))
-  (def d (build-graph [[1 2] [1 3] [3 5] [2 4]]))
-  b
-  (graph/pprint (apply graph/multidigraph edges))
-  (graph/pprint (transitively-reduce-graph (apply graph/multidigraph edges)))
-  (prune d)
-  {1 {:val 1,           :out #{3 2}},
-   2 {:val 2, :in #{1}, :out #{4}},
-   3 {:val 3, :in #{1}, :out #{5}},
-   5 {:val 5, :in #{3}},
-   4 {:val 4, :in #{2}}}
-
-  {1 {:val 1, :out #{3 2}},
-   2 {:val 2, :in #{1}},
-   3 {:val 3, :in #{1}}}
-
-  {1 {:val 1,           :out #{2}},
-   2 {:val 2, :in #{1}, :out #{3}},
-   3 {:val 3, :in #{2}, :out #{4}},
-   4 {:val 4, :in #{3}}}
-
-
-  (1 2 3 4)
-
-  (bfs a 1 3)
-  [1 4 3]
-  nil
-  [3 4]
-  [3 4 nil]
-  [1 4 3]
-  [1 4]
-
-  [1 nil nil nil]
-  (prune a)
-  (([1 3] [2 3]) ([1 4] [3 4] [2 4]))
-  (() ([1 2]) ([1 3] [2 3]) ([1 4] [3 4] [2 4]))
-  (1 2 3 4)
-
-
-  (-> {}
-      (add-edge [1 2])
-      (add-edge [2 3])
-      (add-edge [3 4])
-      (add-edge [1 4]))
-  {1 {:val 1, :out #{4 2}},
-   2 {:val 2, :in #{1}, :out #{3}},
-   3 {:val 3, :in #{2}, :out #{4}},
-   4 {:val 4, :in #{1 3}}}
-
-  (get-path a 1 3)
-  [1 2 3]
-  (get-path a 1 4)
-  [1 4]
-  (get-path a 4 1) ;; should be nil or empty
-  nil
-  [4]
-
-  [1 2 3]
-  [2 3 4]
-
-  [1 2 3]
-  [1 2 3 4]
-  [1 4 2 3]
-  [1 4 2 3]
-
-  [1 2]
-  [1 2 3]
-  [1 1 2]
-
-  )
-
 (defn topo-sort
   "Topographically sort a tree, using the ordering function to provide a consistent total
   order in cases where a node has multiple outgoing edges. Not stack safe. Not type safe."
   ([g]
    ;; this will blow up on non-number collections
-   (topo-sort g <))
-  ([g ord]
-   (topo-sort (prune g) ord (->> (vals g)
+   (topo-sort compare g))
+  ([ord g]
+   (topo-sort ord (prune g) (->> (vals g)
+                                 ;; there should only be one "root" node that doesn't have
+                                 ;; any incoming edges
                                  (filter (complement :in))
                                  (first)
                                  (:val))))
-  ([g ord start]
+  ([ord g start]
    (loop [node (get g start)
           res [start]]
      (let [[next & rst :as children] (:out node)]
@@ -314,7 +224,10 @@
   [1 2 3 4]
 
 
-  ({:val 1, :out #{4 3 2}}))
+  ({:val 1, :out #{4 3 2}})
+
+
+  )
 
 
 ;; a -> b
@@ -330,24 +243,15 @@
 ;; (map first)
 
 (defn relations->seq
-  [{:keys [mem ord]}]
-  ;; step 1 - build directed graph
-  ;; step 2 - reduce graph
-  ;; step 3 - sort it topologically
-  ;; step 4 - apply arbitration edge for consistent order
-  ;; right now I'm just crudely sorting the ord coll to handle step 3
-  ;; ideally topsort should take a comparator function to break ties
-  ;; and apply a total order to the result
-  ;; URG topsort doesn't work with multigraphs
+  [{:keys [mem ord val-map]}]
   (cond
     (empty? mem) '()
     (empty? ord) (seq mem)
     :else
     (->> ord
-         (apply graph/multidigraph)
-         (transitively-reduce-graph)
-         (extra/topsort2)
-         (:topological-order))))
+         (build-graph)
+         (topo-sort)
+         (map val-map))))
 
 (comment
   (seq->relations [1 2 3])
@@ -355,68 +259,75 @@
   (relations->seq (seq->relations [1 2 3]))
   '(1 2 3)
 
+  (->> [[1 2] [1 3] [2 3]]
+       (build-graph))
+
   (relations->seq (seq->relations [1 2 3 4 5]))
+
+  '(1 2 3 4 5)
   '(1 2 3 4 5)
   (relations->seq (seq->relations [1 2 false nil  true]))
-  ;; doesen't work with nil or false
+  '(1 2 false nil true)
 
-  (relations->seq {:mem [1 2 3] :ord #mset [[1 2] [1 3]]})
-  [1 3 2]
-  {:has-cycle? false, :topological-order [1 3 2]}
-  '(1 3 2)
+  (relations->seq (seq->relations [1 1 1]))
+  '(1 1 1)
 
-  (reduce str (relations->seq (seq->relations "helo!")))
-  "helo!"
-
-
-  (seq->relations [1 1 1])
-  {:mem #mset [1 1 1], :ord #mset [[1 1] [1 1] [1 1]]}
-  (relations->seq {:mem #mset [1 1 1], :ord #mset [[1 1] [1 1] [1 1]]})
-  {:has-cycle? true}
-  ;; broken with multigraphs :(
-
-
-
-
+  (relations->seq (seq->relations [1 2 1]))
+  (1 2 1)
   )
 
 (defprotocol Mergeable
   (abstractify [this])
   (three-way-merge [r1 r2 lca]))
 
+(defn product
+  [s1 s2]
+  (set (for [a s1 b s2]
+         [a b])))
+
+(defn three-way-merge-sequence
+  [r1 r2 lca]
+  (let [{lca-mem :mem lca-ord :ord :as lca-a} (abstractify lca)
+        {r1-mem :mem r1-ord :ord :as r1-a} (abstractify r1)
+        {r2-mem :mem r2-ord :ord :as r2-a} (abstractify r2)
+        merged-mem (mset/union (mset/intersection lca-mem r1-mem r2-mem)
+                               (mset/difference r1-mem lca-mem)
+                               (mset/difference r2-mem lca-mem))]
+    (relations->seq {:mem merged-mem
+                     :ord (mset/intersection (mset/union (mset/intersection lca-ord r1-ord r2-ord)
+                                                         (mset/difference r1-ord lca-ord)
+                                                         (mset/difference r2-ord lca-ord))
+                                             ;; the new order can only contain merged members
+                                             (mset/product merged-mem merged-mem))
+                     :val-map (merge (:val-map lca-a) (:val-map r1-a) (:val-map r2-a))})))
+
 (extend-type clojure.lang.Sequential
   Mergeable
   (abstractify [s] (seq->relations s))
   (three-way-merge [r1 r2 lca]
-         (let [{lca-mem :mem lca-ord :ord} (abstractify lca)
-               {r1-mem :mem r1-ord :ord} (abstractify r1)
-               {r2-mem :mem r2-ord :ord} (abstractify r2)
-               merged-mem (mset/union (mset/intersection lca-mem r1-mem r2-mem)
-                                      (mset/difference r1-mem lca-mem)
-                                      (mset/difference r2-mem lca-mem))]
-           (relations->seq {:mem merged-mem
-                            :ord (mset/intersection (mset/union (mset/intersection lca-ord r1-ord r2-ord)
-                                                                (mset/difference r1-ord lca-ord)
-                                                                (mset/difference r2-ord lca-ord))
-                                                    ;; the new order can only contain merged members
-                                                    (mset/product merged-mem merged-mem))}))))
+    (three-way-merge-sequence r1 r2 lca)))
+
+(defn three-way-merge-string
+  [r1 r2 lca]
+  (let [{lca-mem :mem lca-ord :ord} (abstractify lca)
+        {r1-mem :mem r1-ord :ord} (abstractify r1)
+        {r2-mem :mem r2-ord :ord} (abstractify r2)
+        merged-mem (mset/union (mset/intersection lca-mem r1-mem r2-mem)
+                               (mset/difference r1-mem lca-mem)
+                               (mset/difference r2-mem lca-mem))]
+    {:mem merged-mem
+     :ord (set/intersection (set/union (set/intersection lca-ord r1-ord r2-ord)
+                                       (set/difference r1-ord lca-ord)
+                                       (set/difference r2-ord lca-ord))
+                            ;; the new order can only contain merged members
+                            (product merged-mem merged-mem))}))
 
 (extend-type java.lang.String
   Mergeable
   (abstractify [s] (seq->relations s))
   (three-way-merge [r1 r2 lca]
-    (let [{lca-mem :mem lca-ord :ord} (abstractify lca)
-          {r1-mem :mem r1-ord :ord} (abstractify r1)
-          {r2-mem :mem r2-ord :ord} (abstractify r2)
-          merged-mem (mset/union (mset/intersection lca-mem r1-mem r2-mem)
-                                 (mset/difference r1-mem lca-mem)
-                                 (mset/difference r2-mem lca-mem))]
-      (reduce str (relations->seq {:mem merged-mem
-                                   :ord (mset/intersection (mset/union (mset/intersection lca-ord r1-ord r2-ord)
-                                                                       (mset/difference r1-ord lca-ord)
-                                                                       (mset/difference r2-ord lca-ord))
-                                                           ;; the new order can only contain merged members
-                                                           (mset/product merged-mem merged-mem))})))))
+    ;; delegate to Sequential implementation
+    (apply str (three-way-merge (seq r1) (seq r2) (seq lca)))))
 
 (extend-type clojure.lang.IPersistentMap
   Mergeable
@@ -441,17 +352,62 @@
                  (set/difference r1-mem lca-mem)
                  (set/difference r2-mem lca-mem)))))
 
+(defn find-cycle
+  [edges]
+  (reduce #(let [next-g (graph/add-edges %1 %2)]
+             (if (alg/dag? next-g)
+               next-g
+               (reduced [%2 %1])))
+          (graph/digraph)
+          edges))
 
 (comment
+  ;;
   (three-way-merge [1 3] [1 2] [1] )
+
   [1 3 2]
   (three-way-merge '(1 3) '(1 2) '(1) )
   [1 2 3]
   (three-way-merge "helo wurd!" "helo!:D" "helo")
+
   "helo wurd!:D"
+  (three-way-merge "hello world!" "hello! :D" "hello!")
+  ""
+  (def rs (three-way-merge-string "h u!" "h! :D" "h!"))
+  #{[[\space 1] [\u 1]]
+    [[\u 1] [\! 1]]
+    [[\space 1] [\! 1]]
+    [[\h 1] [\u 1]]
+    [[\h 1] [\space 1]]
+    [[\h 1] [\! 1]]}
+  #{[[\! 1] [\: 1]]
+    [[\! 1] [\space 1]]                 ; here
+    [[\space 1] [\: 1]]
+    [[\h 1] [\space 1]]
+    [[\h 1] [\D 1]]
+    [[\h 1] [\: 1]]
+    [[\! 1] [\D 1]]
+    [[\space 1] [\D 1]]
+    [[\h 1] [\! 1]]
+    [[\: 1] [\D 1]]}
+  #{[[\h 1] [\! 1]]}
+  (def ord (seq (:ord rs)))
+
+  (def g (relations->seq rs))
+  ;; dag? false
+
+  (reduce #(let [next-g (graph/add-edges %1 %2)]
+             (if (alg/dag? next-g)
+               (reduced [%2 %1])
+               next-g))
+          (graph/digraph)
+          (:ord g))
+
+
+  (three-way-merge [2] [2] [])
+  '(2 2)
+
   (three-way-merge {:a 1} {:b 2} {:c 2})
   {:b 2, :a 1}
   (three-way-merge #{1 2 3} #{4 5 6} #{1 10})
-  #{4 6 3 2 5}
-
-  )
+  #{4 6 3 2 5})
